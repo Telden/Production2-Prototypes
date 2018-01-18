d6cb7003dd8107167164302184edadde1b192a56
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2018-01-17 19:02:06 -0500

    Grappling hook Protoype

---
+++ Prod2 Prototypes/Assets/Scripts/Grapple.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapple : MonoBehaviour
+{
+	private GameObject player;
+
+	public GameObject pointer;
+	public float pointerSpeed;
+	public LayerMask dashTo;
+	public Vector3 currentPoint;
+
+	void Start ()
+	{
+		player = GameObject.Find("Player");
+	}
+
+	void Update ()
+	{
+		DoPointer();
+		DoInput();
+	}
+
+	private void DoPointer()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.forward, out hit, dashTo))
+		{
+			pointer.transform.position = hit.point;
+
+			if (Input.GetKeyDown(KeyCode.Space)) 
+			{
+				player.GetComponent<Rigidbody>().velocity = (hit.point - transform.position).normalized * pointerSpeed;
+				currentPoint = hit.point;
+			}
+		}
+		else
+		{
+			pointer.transform.position = transform.position;
+		}
+
+		if ((transform.position - currentPoint).magnitude < 2)
+		{
+			player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+		}
+	}
+
+	private void DoInput()
+	{
+		
+	}
+}
---
+++ Prod2 Prototypes/Assets/Scripts/RandomlyGenerate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomlyGenerate : MonoBehaviour
+{
+	public int num;
+	public int maxX, maxY, maxZ;
+	public int minSize, maxSize;
+	public GameObject asteroid;
+
+	void Start ()
+	{
+		Gen();
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	void Gen()
+	{
+		int seedX, seedY, seedZ;
+		float scale;
+		GameObject tmp;
+
+		for (int i = 0; i < num; i++)
+		{
+			seedX = Random.Range(-maxX, maxX);
+			seedY = Random.Range(-maxY, maxY);
+			seedZ = Random.Range(-maxZ, maxZ);
+
+			scale = Random.Range(minSize, maxSize);
+
+			tmp = Instantiate(asteroid, new Vector3(seedX, seedY, seedZ), transform.rotation);
+			tmp.transform.localScale = new Vector3(scale, scale, scale);
+		}
+	}
+}
