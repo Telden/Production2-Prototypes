e2222a807dd40758481654d3d5a4e68082533337
nicholas.robbins <nicholas.robbins@mymail.champlain.edu>
2018-01-21 12:52:31 -0500

    Test scene for preliminary gravity system

---
+++ Prod2 Prototypes/Assets/Scripts/Planet  Gravity/PlayerGravity.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGravity : MonoBehaviour {
+
+	// Use this for initialization
+	public Rigidbody mCurrentSurface; //The current surface that the player is on
+	private Rigidbody mRigidbody;
+	public float mGravity;
+	/* Resource Used https://mikeloscocco.wordpress.com/2015/10/13/mario-galaxy-physics-in-unity/ */
+
+	void Start () {
+		mRigidbody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate()
+	{
+		updateOrientation();
+	}
+
+	void updateOrientation()
+	{
+		Vector3 normal = Vector3.zero;
+		normal = getSurfaceNormal();
+		//Set the rotation of the object to what normal of the ground underneath it is
+		this.transform.localRotation = Quaternion.FromToRotation(this.transform.up, normal) * this.transform.rotation;
+		//Add a gravitational force in the opposite direction of the normal on the ground's surface
+		mRigidbody.AddForce(normal * -mGravity);
+
+	}
+
+	Vector3 getSurfaceNormal()
+	{
+		//Get the distance between the player and the gravity object
+		float distance = Vector3.Distance(this.transform.position, mCurrentSurface.transform.position);
+		Vector3 normal = Vector3.zero;
+
+		//Send a raycast of with the length of the distance between the two objects
+		RaycastHit rayHit;
+		if(Physics.Raycast(this.transform.position, -this.transform.position, out rayHit, distance))
+		{
+			normal = rayHit.normal;
+		}
+
+		//Return the normal to where the raycast hit the surface of the object
+		return normal;
+	}
+
+}
---
+++ Prod2 Prototypes/Assets/Scripts/Planet  Gravity/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float mMovementSpeed;
+	private Rigidbody mRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		mRigidBody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		getInput();
+	}
+
+	void getInput()
+	{
+		//Forwards
+		if(Input.GetAxis("Horizontal") > 0)
+		{
+			mRigidBody.AddForce(transform.forward * mMovementSpeed);
+		}
+		//Backwards
+		else if(Input.GetAxis("Horizontal") < 0)
+		{
+			mRigidBody.AddForce(transform.forward * -mMovementSpeed);
+		}
+		//right
+		if(Input.GetAxis("Vertical") > 0)
+		{
+			mRigidBody.AddForce(transform.right * mMovementSpeed);
+		}
+		else if (Input.GetAxis("Vertical") < 0)
+		{
+			mRigidBody.AddForce(transform.right * -mMovementSpeed);
+		}
+	}
+}
