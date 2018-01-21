43596e733686829564e287e642ce16d092060311
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2018-01-21 01:30:52 -0500

    added punching, artificial gravity, and a rough version of player 2

--- Prod2 Prototypes/Assets/Scripts/CarMovement.cs
+++ Prod2 Prototypes/Assets/Scripts/CarMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CarMovement : MonoBehaviour {
 
 	/*Private Variables*/
-	[SerializeField]
+	/*[SerializeField]
 	private float mCurrentAcceleration = 0;
 	[SerializeField]
 	private float mAccelerationIncrease;
@@ -15,35 +15,70 @@ public class CarMovement : MonoBehaviour {
 	private float mMaxVelocity;
 	private float mDeceleration = 1;
 	[SerializeField]
-	private float mRotationVelocity;
+	private float mRotationVelocity;*/
 
+	[Header("Movement")]
 	public float tmpSpd;
 	public float tmpMaxSpd;
 	public float tmpRot;
+	public float tmpRotLess;
 	public float tmpDrag;
 
+	[Header("Ground")]
+	public LayerMask ground;
+	public bool grounded;
+	public float tmpArtificialGravityForce;
 
+	[Header("Controls")]
+	public string axisHor;
+	public string axisVer;
+	public KeyCode keyRight;
+	public KeyCode keyLeft;
 
-	// Use this for initialization
+	[Header("Attacking")]
+	public GameObject punchRight;
+	public GameObject punchLeft;
+	public float startUp;
+	public float duration;
+	public bool punching;
+
 	void Start ()
 	{
 		GetComponent<Rigidbody>().drag = tmpDrag;
+
+		punchRight.SetActive(false);
+		punchLeft.SetActive(false);
 	}
-	
-	// Update is called once per frame
+
 	void Update ()
 	{
 		//checkInput();
 		tmpMovement();
 		tmpTerminalVelocity();
+		tmpPunch();
+		tmpCheckGround();
+
+		if (!grounded)
+		{
+			tmpApplyArtificialGravity();
+		}
 	}
 
 	void tmpMovement()
 	{
 		// add forward and backward movement using GetAxis
-		GetComponent<Rigidbody>().AddForce(transform.forward * Input.GetAxis("Vertical") * tmpSpd);
-		// rotate the vehicle using GetAxis
-		transform.Rotate(0, Input.GetAxis("Horizontal") * tmpRot, 0);
+		GetComponent<Rigidbody>().AddForce(transform.forward * Input.GetAxis(axisVer) * tmpSpd);
+
+		if (punching)
+		{
+			// rotate the vehicle using GetAxis
+			transform.Rotate(0, Input.GetAxis(axisHor) * tmpRotLess, 0);
+		}
+		else
+		{
+			// rotate the vehicle using GetAxis
+			transform.Rotate(0, Input.GetAxis(axisHor) * tmpRot, 0);
+		}
 
 		Debug.DrawLine(transform.position + transform.forward * 5, transform.position);
 	}
@@ -69,7 +104,63 @@ public class CarMovement : MonoBehaviour {
 		}
 	}
 
-	void checkInput()
+	void tmpCheckGround()
+	{
+		if (Physics.Linecast(transform.position, new Vector3(transform.position.x, transform.position.y - 1.1f, transform.position.z), ground))
+		{
+			grounded = true;
+		}
+		else
+		{
+			grounded = false;
+		}
+	}
+
+	void tmpApplyArtificialGravity()
+	{
+		GetComponent<Rigidbody>().AddForce(transform.up * -1 * tmpArtificialGravityForce);
+	}
+
+	void tmpPunch()
+	{
+		if (Input.GetKeyDown(keyRight) && !punching)
+		{
+			Invoke("DoPunchRight", startUp);
+		}
+
+		if (Input.GetKeyDown(keyLeft) && !punching)
+		{
+			Invoke("DoPunchLeft", startUp);
+		}
+	}
+
+	// doing punches
+		private void DoPunchRight()
+		{
+			punchRight.SetActive(true);
+			punching = true;
+			Invoke("StopPunchRight", duration);
+		}
+		private void DoPunchLeft()
+		{
+			punchLeft.SetActive(true);
+			punching = true;
+			Invoke("StopPunchLeft", duration);
+		}
+
+	// stopping punches
+		private void StopPunchRight()
+		{
+			punching = false;
+			punchRight.SetActive(false);
+		}
+		private void StopPunchLeft()
+		{
+			punching = false;
+			punchLeft.SetActive(false);
+		}
+
+	/*void checkInput()
 	{
 		//Forwards
 		if(Input.GetKey(KeyCode.UpArrow))
@@ -108,5 +199,5 @@ public class CarMovement : MonoBehaviour {
 		{
 			transform.Rotate(0,-mRotationVelocity,0);
 		}
-	}
+	}*/
 }
---
+++ Prod2 Prototypes/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+	public KeyCode rightPunch;
+	public KeyCode leftPunch;
+}
