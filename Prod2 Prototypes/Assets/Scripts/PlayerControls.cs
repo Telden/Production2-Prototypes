92af531788e3e7bdbe0a90e7373ee3d71daeff31
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2018-01-25 16:44:56 -0500

    movement and punching now in different scripts

--- Prod2 Prototypes/Assets/Scripts/PlayerControls.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerControls : MonoBehaviour
-{
-	public KeyCode rightPunch;
-	public KeyCode leftPunch;
-}
---
+++ Prod2 Prototypes/Assets/Scripts/PlayerPunch.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPunch : Player
+{
+	[Header("Attacking")]
+	public GameObject punchHitbox;
+	public Transform[] hitboxLocations;
+	public float startUp;
+	public float duration;
+	public float jumpForce;
+	public bool punching;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		CheckInput();
+	}
+
+	void CheckInput()
+	{
+		if (Input.GetKeyDown(KeyCode.E) && !punching)
+		{
+			Invoke("DoPunchRight", startUp);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Q) && !punching)
+		{
+			Invoke("DoPunchLeft", startUp);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && !punching)
+		{
+			Invoke ("DoPunchJump", startUp);
+		}
+	}
+
+	// doing punches
+	private void DoPunchRight()
+	{
+		punchHitbox.transform.position = hitboxLocations[0].position;
+		punchHitbox.SetActive(true);
+		punching = true;
+		Invoke("StopPunch", duration);
+	}
+
+	private void DoPunchLeft()
+	{
+		punchHitbox.transform.position = hitboxLocations[1].position;
+		punchHitbox.SetActive(true);
+		punching = true;
+		Invoke("StopPunch", duration);
+	}
+
+	private void DoPunchJump()
+	{
+		punchHitbox.transform.position = hitboxLocations[2].position;
+		punchHitbox.SetActive(true);
+		punching = true;
+		rb.AddForce(transform.up * jumpForce);
+		Invoke("StopPunch", duration);
+	}
+
+	// stopping punches
+	private void StopPunch()
+	{
+		punching = false;
+		punchHitbox.SetActive(false);
+	}
+}
