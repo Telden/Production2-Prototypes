b895e585d0573a7351224a0b41bab480bdde399a
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2018-01-30 00:23:14 -0500

    drum is now rolling

---
+++ Prod2 Prototypes/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	[Header("Following")]
+	public GameObject myDrum;
+	public Vector3 followPosition;
+	private Vector3 distance;
+
+	void Start ()
+	{
+		distance = transform.position - myDrum.transform.position;
+	}
+
+	void Update ()
+	{
+		transform.position = new Vector3(myDrum.transform.position.x + distance.x, myDrum.transform.position.y + distance.y, myDrum.transform.position.z + distance.z);
+	}
+}
---
+++ Prod2 Prototypes/Assets/DrumMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DrumMovement : MonoBehaviour
+{
+	private Rigidbody rb;
+
+	[Header("Movement")]
+	public float movSpeed;
+	public float boostForce;
+	public float jumpForce;
+
+	[Header("Being grounded")]
+	public bool grounded;
+	public LayerMask groundMask;
+	public float artificialGravityForce;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update ()
+	{
+		Movement();
+		DrumSticks();
+		CheckGround();
+		ArtificialGravity();
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			SceneManager.LoadScene(0);
+		}
+	}
+
+	void Movement()
+	{
+		rb.AddForce(transform.up * -1 * Input.GetAxis("Horizontal") * movSpeed);
+		if (Input.GetKeyDown(KeyCode.Space) && grounded)
+		{
+			rb.AddForce(Vector3.up * jumpForce);
+		}
+	}
+
+	void DrumSticks()
+	{
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			rb.AddForce(transform.up * boostForce);
+		}
+
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			rb.AddForce(transform.up * boostForce * -1);
+		}
+	}
+
+	void CheckGround()
+	{
+		if (Physics.Raycast(transform.position, Vector3.down, 2.0f, groundMask))
+		{
+			grounded = true;
+		}
+		else
+		{
+			grounded = false;
+		}
+	}
+
+	void ArtificialGravity()
+	{
+		if (!grounded)
+		{
+			rb.AddForce(Vector3.down * artificialGravityForce);
+		}
+	}
+}
